8aba3edd44002d43c056f71639f0f841208a6e23
blastart <blastart@gmail.com>
2010-12-10 08:06:10 -0800

    + From now, you can use this plugin several times within the same page. + Added several new Events and Triggers.

--- js/tag-it.js
+++ js/tag-it.js
@@ -1,100 +1,169 @@
 (function($) {
 
-	$.fn.tagit = function(options) {
+    /*
+     *	@author Levy Carneiro Jr.
+     *  	web: http://levycarneiro.com
+     *  	git: https://github.com/levycarneiro/tag-it
+     *
+     *  Modified by Imre Ardelean at blastart@gmail.com
+     *  	git: https://github.com/blastart/tag-it/blob/master/js/tag-it.js
+     *  New futures:
+     *  	- Object independent. From now, you can use this plugin several times within the same page.
+     *
+     *  	-  new events:
+     *  		- options.removeEvent = function(event, removed_tag_title){ };
+     *  		- options.addEvent = function(event, new_tag_title){ };
+     *  	-  new triggers:
+     *  		- $('ul.your_tagit').trigger('newtag', ['new tag name'])  -> add new tag
+     *  		- $('ul.your_tagit').trigger('newtags', [[array of new tags]]) -> add multiple tags
+     *  			(In this case, the removeEvent() event is not fired.)
+     *  		- $('ul.your_tagit').trigger('remove_all') -> flush all tags (removeEvent() will not be fired)
+    */
 
-		var el = this;
+    $.fn.tagit = function(options) {
+	return this.each(function(i) {
 
-		const BACKSPACE		= 8;
-		const ENTER			= 13;
-		const SPACE			= 32;
-		const COMMA			= 44;
+	    var $ul = $(this).addClass("tagit"),
+		$input_field = $('<li class="tagit-new"></li>'),
+		$input = $('<input class="tagit-input" type="text" />'),
 
-		// add the tagit CSS class.
-		el.addClass("tagit");
+		BACKSPACE = 8,
+		ENTER = 13,
+		SPACE = 32,
+		COMMA = 44
+	    ;
+	    $input_field.append($input);
+	    $ul.html($input_field);
+	    $ul.data('$tag_input', $input);
+	    $input.data('$ul', $ul);
 
-		// create the input field.
-		var html_input_field = "<li class=\"tagit-new\"><input class=\"tagit-input\" type=\"text\" /></li>\n";
-		el.html (html_input_field);
+	    $ul.click(function(e){
+		    var $ul = $(this);
+		    if (e.target.tagName == 'A') {
+			    // Removes a tag when the little 'x' is clicked.
+			    // Event is binded to the UL, otherwise a new tag (LI > A) wouldn't have this event attached to it.
+			    $ul.trigger('event_removed', [$(e.target).next().val()]);
+			    $(e.target).parent().remove();
+		    }
+		    else {
+			    // Sets the focus() to the input field, if the user clicks anywhere inside the UL.
+			    // This is needed because the input field needs to be of a small size.
+			     $ul.data('$tag_input').focus();
+		    }
+	    });
 
-		tag_input		= el.children(".tagit-new").children(".tagit-input");
+	    $input.keypress(function(event){
+		    var $input = $(this);
+		    if (event.which == BACKSPACE) {
+			    if ($input.val() == "") {
+				    // When backspace is pressed, the last tag is deleted.
+				    var $last = $input.data('$ul').children(".tagit-choice:last");
+				    $input.data('$ul').trigger('event_removed', [$last.find('input').val()]);
+				    $last.remove();
+			    }
+		    }
+		    // Comma/Space/Enter are all valid delimiters for new tags.
+		    else if (event.which == COMMA || event.which == SPACE || event.which == ENTER) {
+			    event.preventDefault();
 
-		$(this).click(function(e){
-			if (e.target.tagName == 'A') {
-				// Removes a tag when the little 'x' is clicked.
-				// Event is binded to the UL, otherwise a new tag (LI > A) wouldn't have this event attached to it.
-				$(e.target).parent().remove();
-			}
-			else {
-				// Sets the focus() to the input field, if the user clicks anywhere inside the UL.
-				// This is needed because the input field needs to be of a small size.
-				tag_input.focus();
-			}
-		});
+			    var typed = $input.val();
+			    typed = typed.replace(/,+$/,"");
+			    typed = $.trim(typed);
 
-		tag_input.keypress(function(event){
-			if (event.which == BACKSPACE) {
-				if (tag_input.val() == "") {
-					// When backspace is pressed, the last tag is deleted.
-					$(el).children(".tagit-choice:last").remove();
-				}
-			}
-			// Comma/Space/Enter are all valid delimiters for new tags.
-			else if (event.which == COMMA || event.which == SPACE || event.which == ENTER) {
-				event.preventDefault();
+			    if (typed != "") {
+				    if (is_new (typed, $input)) {
+					    create_choice (typed, true, $input);
+				    }
+				    // Cleaning the input.
+				    $input.val("");
+			    }
+		    }
+	    }).autocomplete({
+		source: function (request, response) {
+		    $.ajax({
+			url: options.availableTags,
+			type: "POST",
+			dataType: "json",
+			data: {
+			     term: request.term
+			},
+			success: response
+		    });
+		},
+		select: function(event,ui){
+		    var $input = $(this);
 
-				var typed = tag_input.val();
-				typed = typed.replace(/,+$/,"");
-				typed = typed.trim();
+		    if (is_new(ui.item.value, $input) && ui.item.value != "") {
+			    create_choice (ui.item.value, true, $input);
+		    }
+		    // Cleaning the input.
+		    $input.val("");
 
-				if (typed != "") {
-					if (is_new (typed)) {
-						create_choice (typed);
-					}
-					// Cleaning the input.
-					tag_input.val("");
-				}
-			}
-		});
-
-		tag_input.autocomplete({
-			source: options.availableTags, 
-			select: function(event,ui){
-				if (is_new (ui.item.value)) {
-					create_choice (ui.item.value);
-				}
-				// Cleaning the input.
-				tag_input.val("");
-
-				// Preventing the tag input to be update with the chosen value.
-				return false;
-			}
-		});
-
-		function is_new (value){
-			var is_new = true;
-			this.tag_input.parents("ul").children(".tagit-choice").each(function(i){
-				n = $(this).children("input").val();
-				if (value == n) {
-					is_new = false;
-				}
-			})
-			return is_new;
+		    return false;
 		}
-		function create_choice (value){
-			var el = "";
-			el  = "<li class=\"tagit-choice\">\n";
-			el += value + "\n";
-			el += "<a class=\"close\">x</a>\n";
-			el += "<input type=\"hidden\" style=\"display:none;\" value=\""+value+"\" name=\"item[tags][]\">\n";
-			el += "</li>\n";
-			var li_search_tags = this.tag_input.parent();
-			$(el).insertBefore (li_search_tags);
-			this.tag_input.val("");
-		}
-	};
+	    });
 
-	String.prototype.trim = function() {
-		return this.replace(/^\s+|\s+$/g,"");
-	};
+	    $ul.bind('newtag', function(e, tag){
+		var $ul = $(this);
+		if ( is_new(tag, $ul.data('$tag_input')) ) {
+			create_choice(tag, true, $ul.data('$tag_input'));
+		}
+
+	    }).bind('newtags', function(e, tags) {
+		var $ul = $(this);
+		for (tag in tags) {
+		    if ( is_new(tags[tag], $ul.data('$tag_input')) ) {
+			    create_choice(tags[tag], false, $ul.data('$tag_input'));
+		    }
+
+		}
+
+	    }).bind('remove_all', function(e){
+
+		$(this).find('.tagit-choice').remove();
+
+	    }).bind('update_data', function(e){
+		var tags = [];
+		$(this).find('.tagit-choice input').each(function(e) {
+		    tags.push($.trim($(this).val()))
+		});
+
+		$(this).data('tags',tags);
+	    });
+
+	    if (typeof options.removeEvent == "function") {
+		$ul.bind('event_removed',options.removeEvent);
+	    }
+	    if (typeof options.addEvent == "function") {
+		$ul.bind('event_added',options.addEvent);
+	    }
+
+	    function is_new (value, $input){
+		    var is_new = true;
+		    $input.parent().parent().children(".tagit-choice").each(function(i){
+			    if (value == $(this).children("input").val()) {
+				    is_new = false;
+			    }
+		    });
+		    return is_new;
+	    }
+	    function create_choice (value, trigger_event, $input){
+
+		    var ds = ((typeof trigger_event == "boolean") ? trigger_event : true)
+			$ul = $input.data('$ul'),
+			new_el  =
+			'<li class="tagit-choice">'+ value + '<a class="close">x</a>'+
+			    '<input type="hidden" style="display:none;" value="'+ value +'" name="item[tags][]">'+
+			'</li>'
+		    ;
+
+		    $(new_el).insertBefore($input.parent());
+		    $input.val("");
+		    if (ds) { $ul.trigger('event_added', [value]); }
+	    }
+
+	});
+    };
+
 
 })(jQuery);
